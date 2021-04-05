2dd9fa9895af406afc04db78fa4f609122a0656e
James Bailey <git@jdb.name>
2021-04-05 18:42:13 -0400

    Initial Commit

---
+++ smartTBW/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  smartTBW
+//
+//  Created by James Bailey on 4/5/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
